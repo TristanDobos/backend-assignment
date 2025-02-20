ccba84733a9244a56c4510c820662008809c6e2d
Tristan Dobos <73593021+TristanDobos@users.noreply.github.com>
2025-02-20 09:20:07 +0100

    Update FormRepository.ts

--- domain/repositories/FormRepository.ts
+++ domain/repositories/FormRepository.ts
@@ -1,13 +1,50 @@
 export class FormRepository {
-    public findOne(
-        // TODO: fill in the arguments and the return type
-    ) {
-        // you dont have to implement the DB part.
+    // Finds all the fields of the form
+    public async find(): Promise<Field[]> {
+        return "Field" as unknown as Field[];
     }
 
-    public save(
-        // TODO: fill in the arguments and the return type
-    ) {
-        // you dont have to implement the DB part.
+    // Finds a form field by its unique ID
+    public async findOne(fieldId: number): Promise<Field> {
+        return "Field" as unknown as Field;
+    }
+
+    // Saves a new form field
+    public async save(field: InputField): Promise<Field> {
+        return "Field" as unknown as Field;
+    }
+
+    public async updateOrder(fieldId: number, newOrder: number): Promise<Field> {
+        return "Field" as unknown as Field;
+    }
+
+    // Deletes a form field
+    public async delete(fieldId: number): Promise<void> {
+        return;
     }
 }
+
+interface InputField {
+    id?: number;
+    label: string;
+    type: 'textual' | 'select';
+    min_length?: number;
+    max_length?: number;
+    options?: string[];
+}
+
+interface Field extends InputField {
+    order: number;
+}
+
+export interface TextualField extends Field {
+    type: 'textual';
+    min_length: number;  // More relevant for textual fields
+    max_length: number;  // More relevant for textual fields
+}
+
+export interface SelectField extends Field {
+    type: 'select';
+    options: string[];  // Required for select fields
+}
+
